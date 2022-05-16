60706b2a805e919865a106a082dfd5dbc3616bb9
Douglas Cândido <douglasmateus1@hotmail.com>
2022-05-16 09:20:50 -0300

    enhancement: Configurei o deploy dos arquivos estáticos necessários para estilizar as páginas. #14

--- development/sabido/sabido/settings.py
+++ development/sabido/sabido/settings.py
@@ -132,9 +132,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = "/var/www/sabido.com.br/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
